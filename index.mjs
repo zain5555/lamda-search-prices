30291e83f361c83a9067d460b466a792a43edefc
zain5555 <zainabedeen.sohail@gmail.com>
2024-06-19 23:16:05 +1000

    Deployment

--- index.mjs
+++ index.mjs
@@ -13,20 +13,41 @@ import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 const dynamo = DynamoDBDocument.from(new DynamoDB());
 
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "ap-southeast-2" });
 
 
 export async function handler(event) {
     console.log(event);
+
+
+    const command = new SendEmailCommand({
+        Destination: {
+          ToAddresses: ["zainsohail96@gmail.com"]
+        },
+        Message: {
+          Body: {
+            Text: { Data: "Test" },
+          },
+    
+          Subject: { Data: "Test Email" },
+        },
+        Source: "zainabedeen.sohail@gmail.com",
+      });
+
+
     try {
-      const params = {
-      TableName : 'history',
-      Item: {
-             "history":11,
-             "blog_author":"Neil harrison",
-             "blog_title":"Microservice"
-        }
-      }
-    await dynamo.put(params)
+    //   const params = {
+    //   TableName : 'history',
+    //   Item: {
+    //          "history":"11",
+    //          "blog_author":"Neil harrison",
+    //          "blog_title":"Microservice"
+    //     }
+    //   }
+    // await dynamo.put(params)
+    let response = await ses.send(command);
+
     return { body: 'Successfully created item!' }
   } catch (err) {
     
