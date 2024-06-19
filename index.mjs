b857677c8bc546424532e69aa137c8d713f11e00
zain5555 <zainabedeen.sohail@gmail.com>
2024-06-19 20:50:38 +1000

    Deployment

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ export const handler = async (event) => {
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
+      body: JSON.stringify('Hello from Zain!'),
     };
     return response;
   };
