dd8a6314dfe6d15b9458a4573772a76213b98595
zain5555 <zainabedeen.sohail@gmail.com>
2024-06-20 01:25:31 +1000

    Api Integration

--- index.mjs
+++ index.mjs
@@ -17,6 +17,16 @@ import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 const ses = new SESClient({ region: "ap-southeast-2" });
 
 
+import axios from 'axios';
+
+
+
+axios(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error('error:' + err));
+
+
 export async function handler(event) {
     console.log(event);
 
@@ -36,6 +46,13 @@ export async function handler(event) {
       });
 
 
+      const url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd';
+      const options = {
+        method: 'GET',
+        headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-XrgbyYz9iFPf71nBJkPh8afu'}
+      };
+
+
     try {
     //   const params = {
     //   TableName : 'history',
@@ -46,7 +63,10 @@ export async function handler(event) {
     //     }
     //   }
     // await dynamo.put(params)
-    let response = await ses.send(command);
+    // let response = await ses.send(command);
+
+    let responseAx = await axios(url, options);
+    console.log(responseAx.json())
 
     return { body: 'Successfully created item!' }
   } catch (err) {
