f732cefdd5812ac4aed7f057c9a3f06aaa53b759
zain5555 <zainabedeen.sohail@gmail.com>
2024-06-20 21:52:42 +1000

    Refactoring

---
+++ constants.mjs
@@ -0,0 +1,9 @@
+export const emailEmptyMessage = "email field cannot be empty";
+export const emailInvalidMessage = "email is invalid";
+export const cryptoCoinEmptyMessage = "cryptoCoin field cannot be empty";
+export const tableName = "searchHistory";
+export const responseSuccessMessage = "Successfully generated Request!";
+
+
+
+
--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import { createRecord } from "./db.mjs";
 import { getCoinPrice } from "./api_gecko_integration.mjs";
 import { sendMail } from "./ses_mail_client.mjs";
+import { emailEmptyMessage, emailInvalidMessage, cryptoCoinEmptyMessage, tableName, responseSuccessMessage } from "./constants.mjs";
+
 import { v4 as uuidv4 } from 'uuid';
 import * as EmailValidator from 'email-validator';
  
@@ -8,18 +10,18 @@ import * as EmailValidator from 'email-validator';
 export async function handler(event) {
     console.log(event);
 
-    let { email, cryptoCoin } = event["data"];
+    let { email, cryptoCoin } = event;
 
     if(email){
         if(!EmailValidator.validate(email)) 
-            return { error: "email field cannot be empty" };
+            return { error: emailInvalidMessage };
     }
     else{
-        return { error: "email field cannot be empty" };
+        return { error: emailEmptyMessage };
     }
 
     if(!cryptoCoin){
-        return { error: "cryptoCoin field cannot be empty" };
+        return { error: cryptoCoinEmptyMessage };
 
     }
 
@@ -34,7 +36,7 @@ export async function handler(event) {
 
     try {
 
-    await createRecord('searchHistory', recordForCreation)
+    await createRecord(tableName, recordForCreation)
 
     let responseAx = await getCoinPrice(cryptoCoin)
 
@@ -42,7 +44,7 @@ export async function handler(event) {
         await sendMail("Crypto Prices", email, cryptoCoin + " is " + responseAx[cryptoCoin]["usd"] );
     }
 
-    return { body: { message : 'Successfully generated Request!', data : responseAx ? responseAx : {}} }
+    return { body: { message : responseSuccessMessage, data : responseAx ? responseAx : {}} }
   } catch (err) {
     
     return { error: err }
